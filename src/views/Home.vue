7df36f0d2f397422f2f9615d840401c1b7e36d21
Justin Lin <justinlin@justins-mbp-5.lan>
2021-02-02 13:35:06 -0800

    ...

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <!-- NAVIGATION -->
+    <div class="navBar center">
+      <el-button type="primary" v-on:click="buttonClick()">CLICK ME</el-button>
+    </div>
+    {{ result }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      result: "",
+    };
+  },
+  methods: {
+    buttonClick() {
+      axios.get("https://jsonplaceholder.typicode.com/todos/").then((res) => {
+        console.log(res);
+        this.result = res;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  margin: 1rem;
+}
+</style>
